f99a2594694aa70ffc6724ec9a535bddae3f6127
Jody Yuantoro <xyzuan@qmail.id>
2020-04-17 11:29:52 +0700

    Update script_build.sh

--- script_build.sh
+++ script_build.sh
@@ -5,13 +5,19 @@ echo ==============================
 echo  Welcome to xyzuanWorkstation
 echo ==============================
 
-# Enviroment
+echo # Mounting my Workspace
+cd /home/xyzuan/havoc
+
+echo # Enviroment
 export KBUILD_BUILD_USER="xyzuan"
 export KBUILD_BUILD_HOST="xyzscape-dedicated"
 export TEMPORARY_DISABLE_PATH_RESTRICTIONS=true
 export SELINUX_IGNORE_NEVERALLOWS=true
 
-# Build ROM
+echo # Cleaning Out Dir
+rm -rf out/target
+
+echo # Building a rom
 . build/envsetup.sh
-lunch ancient_lavender-userdebug
+lunch havoc_lavender-userdebug
 mka bacon -j8
