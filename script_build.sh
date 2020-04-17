d5b55edce1f251808b26ed829f01d28a104cc29e
Jody Yuantoro <xyzuan@qmail.id>
2020-04-17 11:32:26 +0700

    uprev

--- script_build.sh
+++ script_build.sh
@@ -5,13 +5,19 @@ echo ==============================
 echo  Welcome to xyzuanWorkstation
 echo ==============================
 
-# Enviroment
+echo # Mounting my workspace
+cd /home/xyzuan/ancient
+
+echo # Enviroment
 export KBUILD_BUILD_USER="xyzuan"
 export KBUILD_BUILD_HOST="xyzscape-dedicated"
 export TEMPORARY_DISABLE_PATH_RESTRICTIONS=true
 export SELINUX_IGNORE_NEVERALLOWS=true
 
-# Build ROM
+echo # Cleaning out Dir
+rm -rf out/target
+
+echo # Building ROM
 . build/envsetup.sh
 lunch ancient_lavender-userdebug
 mka bacon -j8
